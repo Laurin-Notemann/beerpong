957383e1a554a101879308da31f1800bab362512
Thies Hölting <thies.hoelting@gmail.com>
2024-11-15 22:56:14 +0100

    add static envelope creation

--- api/src/main/java/pro/beerpong/api/control/HealthcheckController.java
+++ api/src/main/java/pro/beerpong/api/control/HealthcheckController.java
@@ -1,6 +1,5 @@
 package pro.beerpong.api.control;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +11,6 @@ import pro.beerpong.api.model.dto.ResponseEnvelope;
 public class HealthcheckController {
     @GetMapping
     public ResponseEntity<ResponseEnvelope<String>> getHealthcheck() {
-        return new ResponseEntity<>(new ResponseEnvelope<>(ResponseEnvelope.Status.OK, 200, "OK", null), HttpStatus.OK);
+        return ResponseEnvelope.ok("OK");
     }
 }
