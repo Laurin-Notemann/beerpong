c6e00d2c52453e49587ff309d37d17eaca69a178
Thies Hölting <thies.hoelting@gmail.com>
2024-11-15 17:16:45 +0100

    spring base project

---
+++ api/src/main/java/pro/beerpong/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package pro.beerpong.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
