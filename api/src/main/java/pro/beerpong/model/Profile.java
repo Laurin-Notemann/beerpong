c096619d2b39f351c7bd4d9357cb2a22e7f39161
Thies Hölting <thies.hoelting@gmail.com>
2024-11-15 20:33:03 +0100

    resolve conflicts

--- api/src/main/java/pro/beerpong/model/Profile.java
+++ api/src/main/java/pro/beerpong/model/Profile.java
@@ -2,21 +2,13 @@ package pro.beerpong.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-<<<<<<< HEAD
-=======
-import java.util.UUID;
->>>>>>> 9b6c396 (Basic entity creation)
 
 @Entity
 @Data
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-<<<<<<< HEAD
     private String id;
-=======
-    private UUID id;
->>>>>>> 9b6c396 (Basic entity creation)
 
     private String name;
     private String profilePicture;
--- api/src/main/java/pro/beerpong/model/RuleMove.java
+++ api/src/main/java/pro/beerpong/model/RuleMove.java
@@ -2,23 +2,16 @@ package pro.beerpong.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-<<<<<<< HEAD
-=======
-import java.util.UUID;
->>>>>>> 9b6c396 (Basic entity creation)
 
 @Entity
 @Data
 public class RuleMove {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-<<<<<<< HEAD
     private String id;
-=======
-    private UUID id;
->>>>>>> 9b6c396 (Basic entity creation)
 
     private String name;
+
     private int multiplicator;
 
     @ManyToOne
