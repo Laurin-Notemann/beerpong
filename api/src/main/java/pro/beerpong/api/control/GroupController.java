42318625728e80fdd444a3dc53e030e0aa2b8dbe
Ole <ole@farwig.de>
2024-11-15 21:02:36 +0100

    typo fix

--- api/src/main/java/pro/beerpong/api/control/GroupController.java
+++ api/src/main/java/pro/beerpong/api/control/GroupController.java
@@ -1,4 +1,4 @@
-package pro.beerpong.api.controll;
+package pro.beerpong.api.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
