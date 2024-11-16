4348dd5b86890e3139c2704c40ba45dbeb646c6c
Johannes Krabbe <github@Krabbe.dev>
2024-11-16 22:38:59 +0100

    app: continue work on group

--- mobile-app/app/createGroupSetName.tsx
+++ mobile-app/app/createGroupSetName.tsx
@@ -2,7 +2,9 @@ import { useRouter } from 'expo-router';
 import React from 'react';
 
 import { useCreateGroupMutation } from '@/api/calls/group/groupHooks';
+import ErrorScreen from '@/components/ErrorScreen';
 import CreateGroupSetName from '@/components/screens/CreateGroupSetName';
+import { useGroupStore } from '@/zustand/group/stateGroupStore';
 import { useCreateGroupStore } from '@/zustand/group/stateCreateGroupStore';
 
 // Route params must be string types since they come from URL
@@ -13,16 +15,22 @@ export interface CreateGroupSetNameParams {
 export default function Page() {
     const router = useRouter();
     const { members, addName } = useCreateGroupStore();
-    const { mutate } = useCreateGroupMutation();
+    const { mutate, data, status } = useCreateGroupMutation();
+    const { addGroup } = useGroupStore();
 
-    console.log(members);
+    if (status === 'success' && data?.data) {
+        if (!data.data.id) {
+            return <ErrorScreen message="Tja da geht wohl was nicht" />;
+        }
+        addGroup(data.data.id);
+        router.push('/');
+    }
 
     return (
         <CreateGroupSetName
             onSubmit={(group) => {
                 addName(group.name);
                 mutate({ name: group.name });
-                router.push('/');
             }}
         />
     );
