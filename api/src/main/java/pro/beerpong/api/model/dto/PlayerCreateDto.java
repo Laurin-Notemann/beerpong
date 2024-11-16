49bf4fe878668b85b6d43930446ff6f16ddca5b9
Thies <40271292+Thiies@users.noreply.github.com>
2024-11-16 20:59:59 +0100

    Player controller (#23)
    * test
    
    * player mapper and controller
    
    * open api
    
    * todo to match controller

--- api/src/main/java/pro/beerpong/api/model/dto/PlayerCreateDto.java
+++ api/src/main/java/pro/beerpong/api/model/dto/PlayerCreateDto.java
@@ -5,8 +5,6 @@ import pro.beerpong.api.model.dao.Profile;
 import pro.beerpong.api.model.dao.Season;
 
 @Data
-public class PlayerDto {
-    private String id;
-    private Profile profile;
-    private Season season;
+public class PlayerCreateDto {
+    private String profileId;
 }
