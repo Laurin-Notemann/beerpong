ac902a6a2857bd28fdab253cf97284df22920b47
Thies Hölting <thies.hoelting@gmail.com>
2024-11-16 22:36:56 +0100

    Automatically create players on group creation

--- api/src/main/java/pro/beerpong/api/model/dto/GroupCreateDto.java
+++ api/src/main/java/pro/beerpong/api/model/dto/GroupCreateDto.java
@@ -2,7 +2,10 @@ package pro.beerpong.api.model.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class GroupCreateDto {
     private String name;
+    private List<String> playerNames;
 }
