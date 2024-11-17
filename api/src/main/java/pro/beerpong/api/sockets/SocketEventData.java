0537b1d1d0642413c024900975d587e5a27874f3
Thies Hölting <thies.hoelting@gmail.com>
2024-11-17 02:23:11 +0100

    match events

--- api/src/main/java/pro/beerpong/api/sockets/SocketEventData.java
+++ api/src/main/java/pro/beerpong/api/sockets/SocketEventData.java
@@ -11,6 +11,8 @@ public class SocketEventData<T> {
     public static final SocketEventData<GroupDto> GROUP_CREATE = new SocketEventData<>(GroupDto.class, SocketEventType.GROUPS, "groupCreate");
     public static final SocketEventData<GroupDto> GROUP_UPDATE = new SocketEventData<>(GroupDto.class, SocketEventType.GROUPS, "groupUpdate");
 
+    public static final SocketEventData<MatchDto> MATCH_UPDATE = new SocketEventData<>(MatchDto.class, SocketEventType.MATCHES, "matchCreate");
+
     private final Class<T> bodyClass;
     private final SocketEventType eventType;
     private final String scope;
