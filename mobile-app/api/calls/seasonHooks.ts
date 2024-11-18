be1af72795cc3da86fb2b64cea16caf1e819ecaf
Linus Bolls <linus.bolls@gmail.com>
2024-11-18 14:15:14 +0100

    refactor: useGroup hook for easy access to current season id

--- mobile-app/api/calls/seasonHooks.ts
+++ mobile-app/api/calls/seasonHooks.ts
@@ -1,9 +1,11 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 
 import { Paths } from '@/openapi/openapi';
+import { useGroupStore } from '@/zustand/group/stateGroupStore';
 
 import { ApiId } from '../types';
 import { useApi } from '../utils/create-api';
+import { useGroupQuery } from './groupHooks';
 
 export const useSeasonQuery = (seasonId: ApiId | null) => {
     const { api } = useApi();
@@ -49,4 +51,23 @@ export const useStartNewSeasonMutation = () => {
             return res?.data;
         },
     });
+};
+
+/**
+ * returns information about the group we're currently in
+ *
+ * mainly used for getting `groupId` and `seasonId` since we need these for so many queries
+ */
+export const useGroup = () => {
+    const { selectedGroupId } = useGroupStore();
+
+    const { data: groupQueryData } = useGroupQuery(selectedGroupId);
+
+    const seasonId = groupQueryData?.data?.activeSeason?.id;
+
+    return {
+        groupId: selectedGroupId,
+        seasonId,
+        group: { ...(groupQueryData ?? {}) },
+    };
 };
--- mobile-app/app/createGroupSetName.tsx
+++ mobile-app/app/createGroupSetName.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
+import Toast from 'react-native-root-toast';
 
 import { useCreateGroupMutation } from '@/api/calls/groupHooks';
-import ErrorScreen from '@/components/ErrorScreen';
 import CreateGroupSetName from '@/components/screens/CreateGroupSetName';
+import { ConsoleLogger } from '@/utils/logging';
 import { useCreateGroupStore } from '@/zustand/group/stateCreateGroupStore';
 import { useGroupStore } from '@/zustand/group/stateGroupStore';
 
 import { useNavigation } from './navigation/useNavigation';
-
-// Route params must be string types since they come from URL
-export interface CreateGroupSetNameParams {
-    members: string; // This will be JSON string of GroupMember[]
-}
 
 export default function Page() {
     const nav = useNavigation();
@@ -20,27 +16,27 @@ export default function Page() {
     const { mutateAsync } = useCreateGroupMutation();
     const { addGroup } = useGroupStore();
 
-    const [error, setError] = React.useState<string | null>(null);
-
     async function onSubmit(group: { name: string }) {
-        addName(group.name);
+        try {
+            addName(group.name);
 
-        const data = await mutateAsync({
-            name: group.name,
-            profileNames: members.map((m) => m.name),
-        });
-        if (!data?.data?.id) {
-            setError('failed to create group');
+            const data = await mutateAsync({
+                name: group.name,
+                profileNames: members.map((m) => m.name),
+            });
+            if (!data?.data?.id) {
+                throw new Error('invalid create group response');
+            }
+            addGroup(data.data.id);
 
-            return;
+            nav.navigate('index');
+        } catch (err) {
+            ConsoleLogger.error('failed to create group:', err);
+            Toast.show('Failed to create group.', {
+                duration: 1500,
+                position: 1,
+            });
         }
-        addGroup(data.data.id);
-
-        nav.navigate('index');
-    }
-
-    if (error) {
-        return <ErrorScreen message={error} />;
     }
     return <CreateGroupSetName onSubmit={onSubmit} />;
 }
