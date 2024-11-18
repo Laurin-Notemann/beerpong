98e10feafb64b50ee7f9a64c6e99a0bc625730b8
Linus Bolls <linus.bolls@gmail.com>
2024-11-18 13:42:08 +0100

    chore: logging utility

---
+++ mobile-app/utils/logging.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+
+export type LogFunction = (...args: unknown[]) => void;
+
+export interface Logger {
+    fatal: LogFunction;
+    error: LogFunction;
+    warn: LogFunction;
+    info: LogFunction;
+    debug: LogFunction;
+    trace: LogFunction;
+}
+
+export const ConsoleLogger: Logger = {
+    fatal: (...args: any[]) => console.error(...args),
+    error: (...args: any[]) => console.error(...args),
+    warn: (...args: any[]) => console.warn(...args),
+    info: (...args: any[]) => console.info(...args),
+    debug: (...args: any[]) => console.debug(...args),
+    trace: (...args: any[]) => console.log(...args),
+};
+
+export class ScopedLogger implements Logger {
+    private prefixes: string[];
+
+    constructor(...prefixes: string[]) {
+        this.prefixes = prefixes;
+    }
+    private getPrefixesString(): string {
+        return this.prefixes.map((i) => `[${i}]`).join('');
+    }
+    public extendScope(prefix: string) {
+        return new ScopedLogger(...this.prefixes, prefix);
+    }
+
+    fatal = (...args: any[]) =>
+        console.error(this.getPrefixesString(), ...args);
+    error = (...args: any[]) =>
+        console.error(this.getPrefixesString(), ...args);
+    warn = (...args: any[]) => console.warn(this.getPrefixesString(), ...args);
+    info = (...args: any[]) => console.info(this.getPrefixesString(), ...args);
+    debug = (...args: any[]) =>
+        console.debug(this.getPrefixesString(), ...args);
+    trace = (...args: any[]) => console.log(this.getPrefixesString(), ...args);
+}
