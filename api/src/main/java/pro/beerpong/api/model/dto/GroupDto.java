8213f433fa863f0761c82498d493e72c7efc0e2a
Laurin-Notemann <laurin.notemann@gmail.com>
2024-11-21 18:27:16 +0100

    test in pipeline

--- api/src/main/java/pro/beerpong/api/model/dto/GroupDto.java
+++ api/src/main/java/pro/beerpong/api/model/dto/GroupDto.java
@@ -1,12 +1,13 @@
 package pro.beerpong.api.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import pro.beerpong.api.model.dao.Season;
 
 @Data
 public class GroupDto {
-    private String id;
+    private @NotNull String id;
     private String name;
     private String inviteCode;
     private Season activeSeason;
