d7868b479250894cbff1d20bf256bd9a6e78e5c0
Linus Bolls <linus.bolls@gmail.com>
2024-11-21 01:37:24 +0100

    chore: initialize sentry

--- mobile-app/api/env.ts
+++ mobile-app/api/env.ts
@@ -48,4 +48,9 @@ export const env = {
         },
     },
     isDev: __DEV__,
+    sentry: {
+        dsn: 'https://48d0726c7648ee3dc2ae1c6f0238d7ed@o4508333440892928.ingest.de.sentry.io/4508333445152848',
+
+        enabled: !__DEV__,
+    },
 };
--- mobile-app/metro.config.js
+++ mobile-app/metro.config.js
@@ -1,7 +1,7 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
 // eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 // we can't include any vitest imports in our bundle because they crash the metro build
 config.resolver.blacklistRE = /.*\.test\.(ts|tsx)$/;
