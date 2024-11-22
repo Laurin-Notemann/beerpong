6c7b51c19f7578f49f44e0e438201cc8950cf229
Linus Bolls <linus.bolls@gmail.com>
2024-11-22 15:15:00 +0100

    feat: haptic feedback for some actions

---
+++ mobile-app/haptics.ts
@@ -0,0 +1,13 @@
+import * as Haptics from 'expo-haptics';
+
+export function triggerHapticBump(
+    action: 'selection' | 'toast:success' | 'toast:error'
+) {
+    if (action === 'toast:success') {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    }
+    if (action === 'toast:error') {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+    }
+    Haptics.selectionAsync();
+}
