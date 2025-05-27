635caaa2fd80c46ceb02bbb6487b0d71d1990f2c
Linus Bolls <linus.bolls@gmail.com>
2025-05-27 11:15:24 +0200

    fix: <RefreshControl> being swallowed by header

--- mobile-app/components/RefreshControl.tsx
+++ mobile-app/components/RefreshControl.tsx
@@ -4,10 +4,16 @@ import { RefreshProps } from '@/api/utils/reactQuery';
 import { theme } from '@/theme';
 
 export const RefreshControl = (props: RefreshProps) => {
+    /**
+     * instead of accounting for the vertical offset due to the header here,
+     * we might want to instead have `contentInset={{ top: headerHeight }} contentOffset={{ y: -headerHeight }}` on the parent `<FlatList>`,
+     * which might be more modular if we ever want to use a `<RefreshControl>` in a list that doesn't have a header
+     */
     return (
         <ReactNativeRefreshControl
             {...props}
             tintColor={theme.refreshControl.tintColor}
+            progressViewOffset={98}
         />
     );
 };
