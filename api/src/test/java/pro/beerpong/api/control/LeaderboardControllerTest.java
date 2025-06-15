dee01e639a9ec4f9f16dbe9a9fe600db697db26e
Thies <thies.hoelting@gmail.com>
2025-06-15 21:38:57 +0200

    leaderboard tests

---
+++ api/src/test/java/pro/beerpong/api/control/LeaderboardControllerTest.java
@@ -0,0 +1,45 @@
+package pro.beerpong.api.control;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import pro.beerpong.api.RequestUtils;
+import pro.beerpong.api.TestUtils;
+import pro.beerpong.api.model.dto.ErrorCodes;
+import pro.beerpong.api.model.dto.GroupCreateDto;
+import pro.beerpong.api.model.dto.GroupDto;
+import pro.beerpong.api.util.DailyLeaderboard;
+import pro.beerpong.api.util.RankingAlgorithm;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class LeaderboardControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RequestUtils requestUtils;
+    @Autowired
+    private TestUtils testUtils;
+
+    @Test
+    @Transactional
+    public void leaderboard_get_success() {
+        
+    }
+
+    @Test
+    public void leaderboard_get_invalidScope() {
+
+    }
+}
