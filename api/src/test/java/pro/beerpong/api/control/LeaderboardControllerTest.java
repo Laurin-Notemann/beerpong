ff90a271f85e9f7328bc416bd036ebb54136865c
Thies <thies.hoelting@gmail.com>
2025-06-17 23:42:14 +0200

    first leaderboard tests

--- api/src/test/java/pro/beerpong/api/control/LeaderboardControllerTest.java
+++ api/src/test/java/pro/beerpong/api/control/LeaderboardControllerTest.java
@@ -12,6 +12,7 @@ import pro.beerpong.api.TestUtils;
 import pro.beerpong.api.model.dto.ErrorCodes;
 import pro.beerpong.api.model.dto.GroupCreateDto;
 import pro.beerpong.api.model.dto.GroupDto;
+import pro.beerpong.api.model.dto.LeaderboardDto;
 import pro.beerpong.api.util.DailyLeaderboard;
 import pro.beerpong.api.util.RankingAlgorithm;
 
@@ -34,12 +35,38 @@ public class LeaderboardControllerTest {
 
     @Test
     @Transactional
-    public void leaderboard_get_success() {
+    public void leaderboard_get_Alltime_empty() {
+        var profileNames = List.of("player1", "player2", "player3");
+        var prerequisiteGroup = testUtils.createTestGroup(port, "test", profileNames);
 
+        var response = requestUtils.performGet(port, "/groups/" + prerequisiteGroup.getId() + "/leaderboard?scope=all-time", LeaderboardDto.class);
+        var leaderboard = requestUtils.assertSuccess(response, LeaderboardDto.class);
+
+        assertEquals(0, leaderboard.getNumMatches());
+        assertEquals(3, leaderboard.getNumPlayers());
+        assertEquals(prerequisiteGroup.getCreatedAt().toEpochSecond(), leaderboard.getStartedAt().toEpochSecond());
     }
 
     @Test
     public void leaderboard_get_invalidScope() {
+        var prerequisiteGroup = testUtils.createTestGroup(port, "test");
 
+        // test invalid scope (non provided)
+        var response = requestUtils.performGet(port, "/groups/" + prerequisiteGroup.getId() + "/leaderboard?scope=something", LeaderboardDto.class);
+        requestUtils.assertFailure(response, ErrorCodes.LEADERBOARD_SCOPE_NOT_FOUND);
+
+        // test invalid scope (scope=season without season id)
+        response = requestUtils.performGet(port, "/groups/" + prerequisiteGroup.getId() + "/leaderboard?scope=season", LeaderboardDto.class);
+        requestUtils.assertFailure(response, ErrorCodes.LEADERBOARD_SEASON_NOT_FOUND);
+
+        // test invalid scope (invalid season id)
+        response = requestUtils.performGet(port, "/groups/" + prerequisiteGroup.getId() + "/leaderboard?scope=season&seasonId=someId", LeaderboardDto.class);
+        requestUtils.assertFailure(response, ErrorCodes.SEASON_NOT_FOUND);
+
+        var prerequisiteGroup1 = testUtils.createTestGroup(port, "test123");
+
+        // test invalid scope (season id from other group)
+        response = requestUtils.performGet(port, "/groups/" + prerequisiteGroup.getId() + "/leaderboard?scope=season&seasonId=" + prerequisiteGroup1.getActiveSeason().getId(), LeaderboardDto.class);
+        requestUtils.assertFailure(response, ErrorCodes.SEASON_NOT_OF_GROUP);
     }
 }
