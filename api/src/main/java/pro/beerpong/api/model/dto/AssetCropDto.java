153ec1beb854a4b101dd6db656b781171183d9d7
Laurin-Notemann <laurin.notemann@gmail.com>
2025-09-22 12:00:53 +0200

    test

--- api/src/main/java/pro/beerpong/api/model/dto/AssetCropDto.java
+++ api/src/main/java/pro/beerpong/api/model/dto/AssetCropDto.java
@@ -10,6 +10,8 @@ public class AssetCropDto {
     private double offsetX;
     private double offsetY;
     private double zoom;
+    private double width;
+    private double height;
 
     public boolean validate() {
         return this.offsetX >= 0 && this.offsetY >= 0 && this.zoom >= 0;
