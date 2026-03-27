9d663ef08ef5544ede2c9cc7b174693099ac50f8
thies <thies.hoelting@gmail.com>
2026-03-27 03:47:21 +0100

    better application properties

--- api/src/main/java/pro/beerpong/api/ApiApplication.java
+++ api/src/main/java/pro/beerpong/api/ApiApplication.java
@@ -2,8 +2,11 @@ package pro.beerpong.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "pro.beerpong.api.repository")
 public class ApiApplication {
 
     public static void main(String[] args) {
