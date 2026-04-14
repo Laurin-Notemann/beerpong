3911582a1104d0c6195d74b4ce69f9b4af85efda
Laurin <47634664+Laurin-Notemann@users.noreply.github.com>
2026-04-14 13:50:30 +0200

    Devops/update workflows (#379)
    * chore: run workflow when new changes to api have been made on after already generating types
    
    * test
    
    * chore: update openapi types
    
    * test
    
    * test
    
    * chore: always run then check if api changes
    
    * test
    
    * fix
    
    * chore: update openapi types
    
    * chore: update openapi types
    
    * fix
    
    * chore: update openapi types
    
    * test
    
    * fix
    
    * test
    
    * test
    
    ---------
    
    Co-authored-by: Thies <40271292+Thiies@users.noreply.github.com>

--- mobile-app/openapi/openapi.d.ts
+++ mobile-app/openapi/openapi.d.ts
@@ -12,6 +12,9 @@ declare namespace Components {
             offsetX?: number; // double
             offsetY?: number; // double
             zoom?: number; // double
+            width?: number; // double
+            height?: number; // double
+            originX?: number; // double
         }
         export interface AssetMetadataDto {
             id?: string;
